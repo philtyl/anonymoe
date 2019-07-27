77e1a50b62f136ab70a5f615b628bab5e3d7fd32
philtyl <admin@guantana.moe>
2019-07-27 01:35:43 -0400

    Adding necessary CSS framework and renaming on templates

--- cmd/web.go
+++ cmd/web.go
@@ -1,18 +1,36 @@
 package cmd
 
 import (
+	"path"
+
 	"github.com/urfave/cli"
+	"gopkg.in/macaron.v1"
 )
 
 var Web = cli.Command{
-	Name:  "web",
-	Usage: "Start web server",
+	Name:        "web",
+	Usage:       "Start web server",
 	Description: `Anonymoe web server starts all necessary components`,
-	Action: runWeb,
+	Action:      runWeb,
 	Flags: []cli.Flag{
 		stringFlag("port, p", "3000", "Temporary port number to prevent conflict"),
 		stringFlag("config, c", "custom/conf/app.ini", "Custom configuration file path"),
 	},
+}
+
+func newMacaron() *macaron.Macaron {
+	m := macaron.New()
+	m.Use(macaron.Logger())
+	m.Use(macaron.Static(
+		path.Join(setting.StaticRootPath, "public"),
+	))
+
+	funcMap := template.NewFuncMap()
+	m.Use(macaron.Renderer(macaron.RenderOptions{
+		Directory:  path.Join(setting.StaticRootPath, "templates"),
+		Funcs:      funcMap,
+		IndentJSON: macaron.Env != macaron.PROD,
+	}))
 }
 
 func runWeb(c *cli.Context) error {
---
+++ pkg/setting/setting.go
@@ -0,0 +1,10 @@
+package setting
+
+var (
+	// App settings
+	AppVer      string
+	AppName     string
+	AppURL      string
+	AppPath     string
+	AppDataPath string
+)
