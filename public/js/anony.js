9a7418640a72b51b017fc6d27f2736d081011866
philtyl <admin@guantana.moe>
2019-08-29 17:50:37 -0400

    Fix same origin iframe resize

--- public/js/anony.js
+++ public/js/anony.js
@@ -30,10 +30,6 @@ function registerClipBoard() {
     });
 }
 
-function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-}
-
 function registerInboxFeed() {
     if ($('#inboxfeed').length === 0) {
         return;
